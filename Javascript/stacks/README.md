5ab737c33446854dfb7a521c1f3a238adf8e4408
Jorge A Nunez <1697540+janvmusic@users.noreply.github.com>
2026-03-25 14:27:48 -0600

    Add stacks/medium/sunset-views

--- Javascript/stacks/README.md
+++ Javascript/stacks/README.md
@@ -3,3 +3,4 @@
 ## Medium
 
 1. [Sort Stack](./medium/sort-stack/)
+2. [Sunset Views](./medium/sunset-views/)
---
+++ Javascript/stacks/medium/sunset-views/index.js
@@ -0,0 +1,60 @@
+// Solution 1
+// Step one -> Create a stack
+// Step two -> Iterate through the direction
+// Step three -> iterate to remove smaller or equals
+// Step four -> add to stack
+// Step five -> reverse the result
+function sunsetViewsV1(buildings, direction) {
+  let sunsetViews = [];
+  let idx = direction === 'EAST' ? 0 : buildings.length - 1;
+  let step = direction === 'EAST' ? 1 : -1;
+
+  while (idx >= 0 && idx < buildings.length) {
+    let building = buildings[idx];
+    while (sunsetViews.length > 0 && building >= buildings[sunsetViews[sunsetViews.length - 1]]) {
+      sunsetViews.pop();
+    }
+
+    sunsetViews.push(idx);
+    idx = idx + step;
+  }
+
+  if (direction === 'WEST') {
+    sunsetViews.reverse();
+  }
+
+  return sunsetViews;
+}
+
+// Solution 2
+// Step one -> Iterate the opposite direction
+// Step two -> save the current max
+// Step three -> if curr > tallest, then add it
+// Step four -> reverse the result
+function sunsetViewsV2(buildings, direction) {
+  let sunsetViews = [];
+  let idx = direction === 'EAST' ? buildings.length - 1 : 0;
+  let step = direction === 'EAST' ? -1 : 1;
+  let tallest = -Infinity;
+
+  while (idx >= 0 && idx < buildings.length) {
+    let building = buildings[idx];
+
+    if (building > tallest) {
+      sunsetViews.push(idx);
+      tallest = building;
+    }
+
+    idx = idx + step;
+  }
+
+  if (direction === 'EAST') {
+    sunsetViews.reverse();
+  }
+
+  return sunsetViews;
+}
+
+// Do not edit the line below.
+exports.sunsetViewsV1 = sunsetViewsV1;
+exports.sunsetViewsV2 = sunsetViewsV2;
