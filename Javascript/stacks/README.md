2eb8c71c70496276f9ecb0b808a2f357c10bae8f
Jorge A Nunez <1697540+janvmusic@users.noreply.github.com>
2026-03-25 14:34:10 -0600

    Add stacks/medium/min-max-stack-construction

--- Javascript/stacks/README.md
+++ Javascript/stacks/README.md
@@ -5,3 +5,4 @@
 1. [Sort Stack](./medium/sort-stack/)
 2. [Sunset Views](./medium/sunset-views/)
 3. [Balanced Brackets](./medium/balanced-brackets/)
+4. [Min Max Stack Construction](./medium/min-max-stack-construction/)
---
+++ Javascript/stacks/medium/min-max-stack-construction/index.js
@@ -0,0 +1,38 @@
+// Feel free to add new properties and methods to the class.
+class MinMaxStack {
+  constructor() {
+    this.stack = [];
+    this.minMaxStack = [];
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  pop() {
+    this.minMaxStack.pop();
+    return this.stack.pop();
+  }
+
+  push(number) {
+    let minMax = this.minMaxStack[this.minMaxStack.length - 1] || [Infinity, -Infinity];
+    let min = Math.min(minMax[0], number);
+    let max = Math.max(minMax[1], number);
+
+    this.minMaxStack.push([min,max]);
+    this.stack.push(number);
+  }
+
+  getMin() {
+    let minMax = this.minMaxStack[this.minMaxStack.length - 1];
+    return minMax[0];
+  }
+
+  getMax() {
+    let minMax = this.minMaxStack[this.minMaxStack.length - 1];
+    return minMax[1];
+  }
+}
+
+// Do not edit the line below.
+exports.MinMaxStack = MinMaxStack;
