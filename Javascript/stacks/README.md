86746abaacde05b2c4df3c09e12682136bb9848d
Jorge A Nunez <1697540+janvmusic@users.noreply.github.com>
2026-03-26 19:26:12 -0600

    Add stacks/medium/reverse-polish-notation

--- Javascript/stacks/README.md
+++ Javascript/stacks/README.md
@@ -7,3 +7,4 @@
 3. [Balanced Brackets](./medium/balanced-brackets/)
 4. [Min Max Stack Construction](./medium/min-max-stack-construction/)
 5. [Next Greater Element](./medium/next-greater-element/)
+6. [Reverse Polish Notation](./medium/reverse-polish-notation/)
---
+++ Javascript/stacks/medium/reverse-polish-notation/index.js
@@ -0,0 +1,38 @@
+function reversePolishNotation(tokens) {
+  let stack = [];
+
+  // We need at least 3 items
+  if (tokens.length < 3) {
+    return Number(tokens.pop());
+  }
+
+  for (let token of tokens) {
+    if (["+", "-", "/", "*"].includes(token)) {
+      let numberB = Number(stack.pop());
+      let numberA = Number(stack.pop());
+
+      let opRes = doOperation(numberA, numberB, token);
+      stack.push(opRes);
+    } else {
+      stack.push(token);
+    }
+  }
+
+  return stack.pop();
+}
+
+function doOperation(numberA, numberB, operand) {
+  switch(operand) {
+    case "+":
+      return numberA + numberB;
+    case "-":
+      return numberA - numberB;
+    case "*":
+      return numberA * numberB;
+    case "/":
+      return Math.trunc(numberA / numberB);
+  }
+}
+
+// Do not edit the line below.
+exports.reversePolishNotation = reversePolishNotation;
